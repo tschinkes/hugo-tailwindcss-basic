3b7010a576d793b17cfe0617a98ba5173c047c3a
tschinkes <t-kraft@live.de>
2024-06-11 10:45:22 +0200

    css

--- themes/tw/assets/scss/tailwind.config.js
+++ themes/tw/assets/scss/tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     "./themes/**/layouts/**/*.html",
     "./layouts/**/*.html"
   ],
   theme: {
-    extend: {},
+    /** Komplette Veränderungen hier direkt z.B. colors: */
+    extend: {
+      /**extend erweitert Tailwind */
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        nsky: '#0f1e3c',
+      },
+    },
   },
   plugins: [],
 }
